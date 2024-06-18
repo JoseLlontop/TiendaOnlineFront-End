6de7b6e3fda16d8f7175f9d107c4972c21f72a5b
JoseLlontop <josellontop100@gmail.com>
2024-06-18 11:25:33 -0300

    Refactor de codigo y se cambio la validacion de la url de la imagen

--- src/pages/GestionProductos.jsx
+++ src/pages/GestionProductos.jsx
@@ -10,170 +10,124 @@ import { useFetchData } from "../hooks/useFetchData"
 import 'postcss';
 import axios from 'axios';
 
-
 export const GestionProductos = () => {
-
   const URL = import.meta.env.VITE_API_URL;
 
-  //Desustructuramos los parametros que estamos recibiendo:
-  const { data, isLoading } = useFetchData(`${URL}/api/productos`)
+  const { data, isLoading } = useFetchData(`${URL}/api/productos`);
 
-  //Manejar el estado en componentes funcionales
-  const [productos, setProductos] = useState([])
-  const [id, setId] = useState("")
-  const [nombre, setNombre] = useState("")
-  const [descripcion, setDescripcion] = useState("")
-  const [precio, setPrecio] = useState("")
-  const [imagen, setImagen] = useState("")
-  const [operacion, setOperacion] = useState(1)
-  const [titulo, setTitulo] = useState("")
+  const [productos, setProductos] = useState([]);
+  const [id, setId] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [imagen, setImagen] = useState("");
+  const [operacion, setOperacion] = useState(1);
+  const [titulo, setTitulo] = useState("");
 
-  //Una vez que se redenderice la pagina cargue todos los productos
   useEffect(() => {
     if (!isLoading) {
       setProductos(data);
-      console.log(data)
+      console.log(data);
     }
   }, [data, isLoading]);
 
   const openModal = (op, id, nombre, descripcion, precio, imagen) => {
-    //Cuando se abre el modal seteamos los valores:  
-    setId('')
-    setNombre('')
-    setDescripcion('')
-    setPrecio('')
-    setImagen('')
-    setOperacion(op)
+    setId('');
+    setNombre('');
+    setDescripcion('');
+    setPrecio('');
+    setImagen('');
+    setOperacion(op);
 
-    //si la operacion es 1 el titulo cambia
     if (op == 1) {
-      setTitulo('Registrar Producto')
+      setTitulo('Registrar Producto');
+    } else if (op == 2) {
+      setTitulo('Editar Producto');
+      setId(id);
+      setNombre(nombre);
+      setDescripcion(descripcion);
+      setPrecio(precio);
+      setImagen(imagen);
     }
-    else if (op == 2) {
-      setTitulo('Editar Producto')
-      setId(id)
-      setNombre(nombre)
-      setDescripcion(descripcion)
-      setPrecio(precio)
-      setImagen(imagen)
-    }
-    //Encendemos el primer foto del formulario
-    // Establece un temporizador para que la siguiente acción se ejecute después de un cierto tiempo (en milisegundos)
-    window.setTimeout(
-      function () {
-        // Dentro de la función que se ejecutará después del tiempo especificado
-        // Obtiene el elemento del DOM con el ID 'nombre'
-        document.getElementById('nombre').focus();
 
-        // La función focus() se utiliza para darle el foco (hacer que se seleccione) al elemento, 
-        // en este caso, al elemento con el ID 'nombre'.
-        // Esto significa que, después de 500 milisegundos (o 0.5 segundos), el cursor se ubicará en el campo de entrada con el ID 'nombre'.
-      }, 500
-    )
+    window.setTimeout(() => {
+      document.getElementById('nombre').focus();
+    }, 500);
   }
 
   const validar = () => {
+    let productoParametro;
+    let metodo;
+    let direccion;
 
-    var productoParametro;
-    var metodo;
-    var URL;
-
-    //Validamos los campos antes de enviar la solicitud
-    if (nombre.trim() == '') {
-      show_alerta('Escriba el nombre del producto', 'warning')
-    }
-    else if (precio.trim() == '') {
-      show_alerta('Escribe el precio del producto', 'warning')
-    }
-    else if (imagen.trim() == '') {
-      show_alerta('Escriba la URL de la imagen', 'warning')
-    }
-
-    else {
-      //Revisamos cual es la operacion actual
-      if (operacion == 1) { //1 es para guardar (POST)
-        //Creamos el objeto
-        productoParametro = { nombre: nombre.trim(), descripcion: descripcion.trim(), precio: precio.trim(), imagen: imagen.trim() }
-        metodo = 'POST'
-        URL = `${URL}/api/productos`
+    if (nombre.trim() === '') {
+      show_alerta('Escriba el nombre del producto', 'warning');
+    } else if (precio.trim() === '') {
+      show_alerta('Escribe el precio del producto', 'warning');
+    } else if (!imagen) {
+      show_alerta('Seleccione una imagen para el producto', 'warning');
+      
+    } else {
+      if (operacion === 1) {
+        productoParametro = { nombre: nombre.trim(), descripcion: descripcion.trim(), precio: precio.trim(), imagen: imagen.trim() };
+        metodo = 'POST';
+        direccion = `${URL}/api/productos`;
+      } else {
+        productoParametro = { id: id, nombre: nombre.trim(), descripcion: descripcion.trim(), precio: precio.trim(), imagen: imagen.trim() };
+        metodo = 'PUT';
+        direccion = `${URL}/api/productos/${productoParametro.id}`;
       }
-      else { //2 es para actualizar (PUT)
-        productoParametro = { id: id, nombre: nombre.trim(), descripcion: descripcion.trim(), precio: precio.trim(), imagen: imagen.trim() }
-        metodo = 'PUT'
-        URL = `${URL}/api/productos/${productoParametro.id}`
-      }
-      //Una vez que se asigne el parametro y el metodo de acuerdo a la operacion, enviamos la solicitud
-      enviarSolicitud(metodo, productoParametro, URL)
+      enviarSolicitud(metodo, productoParametro, direccion);
     }
   }
 
-  // Definición de la función enviarSolicitud
   const enviarSolicitud = async (metodo, parametro, direccion) => {
-    // Realiza una solicitud usando axios
-    await axios({ method: metodo, url: direccion, data: parametro }).then(function (respuesta) {
-
-      // Verificar el código de estado HTTP
+    await axios({ method: metodo, url: direccion, data: parametro }).then((respuesta) => {
       if (respuesta.status === 200) {
         document.getElementById('btncerrar').click();
 
-        // Recargar los datos desde el servidor
         if (metodo === 'DELETE') {
-          show_alerta('Produto eliminado', 'success')
-          document.getElementById('btncerrar').click()
-          setProductos(productos.filter(producto => producto.id !== id))
-
+          show_alerta('Producto eliminado', 'success');
+          setProductos(productos.filter(producto => producto.id !== id));
         } else if (metodo === 'POST') {
-          // Si la operación es de agregar, actualiza el estado agregando el nuevo producto
-          show_alerta('Producto guardado', 'success')
-          document.getElementById('btncerrar').click()
-          setProductos([...productos, parametro])
-
+          show_alerta('Producto guardado', 'success');
+          setProductos([...productos, parametro]);
         } else {
-          // Si la operación es de agregar, actualiza el estado agregando el nuevo producto
-          show_alerta('Producto actualizado', 'success')
-          document.getElementById('btncerrar').click()
-          setProductos([...productos, parametro])
+          show_alerta('Producto actualizado', 'success');
+          setProductos(productos.map(p => (p.id === parametro.id ? parametro : p)));
         }
       }
-    }).catch(function (error) {
-      // Si ocurre un error, muestra una alerta de error y loguea el error en la consola
+    }).catch((error) => {
       show_alerta('Error en la solicitud', 'error');
       console.log(error);
-    })
+    });
   }
 
   const deleteProducto = (id, nombre) => {
-    // Crea una instancia de SweetAlert2 con soporte para React
-    const MySwal = withReactContent(Swal)
+    const MySwal = withReactContent(Swal);
 
-    // Muestra una alerta con el mensaje y el icono proporcionados y ...
     MySwal.fire({
-      title: '¿Seguro de eliminar el producto' + nombre + ' ?',
+      title: `¿Seguro de eliminar el producto ${nombre}?`,
       icon: 'question',
-      text: 'No se podra dar marcha atras',
+      text: 'No se podrá dar marcha atrás',
       showCancelButton: true,
-      confirmButtonText: 'Si, eliminar',
+      confirmButtonText: 'Sí, eliminar',
       cancelButtonText: 'Cancelar'
     }).then((respuesta) => {
       if (respuesta.isConfirmed) {
-        setId(id)
-        enviarSolicitud('DELETE', { id: id }, `${URL}/api/productos/${id}`)
+        setId(id);
+        enviarSolicitud('DELETE', { id: id }, `${URL}/api/productos/${id}`);
+      } else {
+        show_alerta('El producto NO fue eliminado', 'info');
       }
-      else {
-        show_alerta('El producto NO fue elimnado', 'info')
-      }
-    })
+    });
   }
 
-  // Función para manejar la carga de archivos
   const handleFileChange = async (e) => {
     const file = e.target.files[0];
     if (!file) return;
 
-    //Envio el archivo al backend para procesarlo y obtener la URL pública de la imagen
     try {
-
-      //Esto nos permite construir fácilmente un conjunto de pares clave/valor que se pueden enviar en una solicitud HTTP
       const formData = new FormData();
       formData.append('file', file);
 
@@ -184,7 +138,6 @@ export const GestionProductos = () => {
 
       const data = await response.json();
       
-      // Actualiza el estado de la imagen con la URL pública obtenida del backend
       setImagen(data.url);
 
       console.log("Imagen recibida y guardada correctamente en el BUCKET: ", data.url);
@@ -209,9 +162,7 @@ export const GestionProductos = () => {
         <div className='row mt-3'>
           <div className='col-12 col-lg-8 offset-lg-2'>
             <div className='table-responsive'>
-              {/* Corregir la etiqueta de apertura de la tabla */}
               <table className='table table-bordered'>
-                {/* Encabezado de la tabla */}
                 <thead>
                   <tr>
                     <th>#</th>
@@ -221,21 +172,18 @@ export const GestionProductos = () => {
                     <th></th>
                   </tr>
                 </thead>
-                {/* Cuerpo de la tabla */}
                 <tbody className='table-group-divider'>
                   {productos.map((producto, i) => (
-                    <tr key={producto.nombre}>
-                      <td>{(i + 1)}</td>
+                    <tr key={producto.id}>
+                      <td>{i + 1}</td>
                       <td>{producto.nombre}</td>
                       <td>{producto.descripcion}</td>
                       <td>${new Intl.NumberFormat("es-mx").format(producto.precio)}</td>
                       <td className='d-flex align-items-center'>
-                        {/* Editar */}
                         <button onClick={() => openModal(2, producto.id, producto.nombre, producto.descripcion, producto.precio, producto.imagen)}
                           className='btn btn-warning me-2' data-bs-toggle='modal' data-bs-target='#modalProductos'>
                           <i className='fa-solid fa-edit'></i>
                         </button>
-                        {/* Eliminar */}
                         <button onClick={() => deleteProducto(producto.id, producto.nombre)} className='btn btn-danger'>
                           <i className='fa-solid fa-trash'></i>
                         </button>
@@ -249,24 +197,16 @@ export const GestionProductos = () => {
         </div>
       </div>
 
-
-      {/* Ventana superpuesta que se utiliza para mostrar información adicional */}
       <div id='modalProductos' className='modal fade' aria-hidden='true'>
         <div className='modal-dialog'>
           <div className='modal-content'>
-            {/* Encabezado del modal */}
             <div className='modal-header'>
-              {/* Título del modal */}
               <label className='h5'>{titulo}</label>
-              {/* Botón de cierre del modal */}
               <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='close'></button>
             </div>
-            {/* Cuerpo del modal */}
             <div className='modal-body'>
-              {/* Input oculto para el id */}
               <input type='hidden' id='id'></input>
 
-              {/* Input de nombre */}
               <div className='input-group mb-3'>
                 <span className='input-group-text'><i className='fa-solid fa-gift'></i></span>
                 <input
@@ -279,7 +219,6 @@ export const GestionProductos = () => {
                 ></input>
               </div>
 
-              {/* Input de descripción */}
               <div className='input-group mb-3'>
                 <span className='input-group-text'><i className='fa-solid fa-comment'></i></span>
                 <input
@@ -292,7 +231,6 @@ export const GestionProductos = () => {
                 ></input>
               </div>
 
-              {/* Input de precio */}
               <div className='input-group mb-3'>
                 <span className='input-group-text'><i className='fa-solid fa-dollar-sign'></i></span>
                 <input
@@ -305,15 +243,14 @@ export const GestionProductos = () => {
                 ></input>
               </div>
 
-              {/* Input de imagen */}
               <div className='input-group mb-3'>
                 <span className='input-group-text'><i className='fa-solid fa-image'></i></span>
                 <input
                   type='file'
                   id='imagen'
                   className='form-control'
-                  accept='image/*' // Acepta cualquier tipo de imagen
-                  onChange={handleFileChange} // Maneja el archivo
+                  accept='image/*'
+                  onChange={handleFileChange}
                 ></input>
               </div>
 
@@ -332,12 +269,9 @@ export const GestionProductos = () => {
                 style={{ backgroundColor: 'gray' }}>Cerrar
               </button>
             </div>
-
-
           </div>
         </div>
       </div>
-
     </div>
   )
 }
